13213d2262677955510aeac298e1a43e29e434a4
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-06-08 14:06:56 +0900

    TDD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO article(id,title,content) values (1,'홍길동',123);
-INSERT INTO article(id,title,content) values (2,'토니 스타크',456);
-INSERT INTO article(id,title,content) values (3,'울버린',789);
-INSERT INTO article(id,title,content) values (4,'쥬비에',012);
+INSERT INTO article(id,title,content) values (1,'H','123');
+INSERT INTO article(id,title,content) values (2,'T','456');
+INSERT INTO article(id,title,content) values (3,'W','789');
+INSERT INTO article(id,title,content) values (4,'J','012');
---
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,85 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ArticleServiceTest {
+    @Autowired ArticleService artcleServie;
+
+
+    @Test
+    void index() {
+
+        List<Article> expected = new ArrayList<>(Arrays.asList(new Article(1L,"H","123"),new Article(2L,"T","456"),new Article(3L,"W","789"),new Article(4L,"J","012")));
+
+        //실제
+        List<Article> articles = artcleServie.index();
+
+        //비교
+        assertEquals(expected.toString(),articles.toString());
+    }
+
+    @Test
+    void show_sucess() {
+        //예상
+        Long id = 1L;
+        Article expected = new Article(id,"H","123");
+        //실제
+        Article article = artcleServie.show(id);
+        //비교
+        assertEquals(expected.toString(),article.toString());
+    }
+
+    @Test
+    void show_fail() {
+        //예상
+        Long id = -1L;
+        Article expected = null;
+        //실제
+        Article article = artcleServie.show(id);
+        //비교
+        assertEquals(expected,article);
+    }
+
+    @Test
+    void create_sucess() {
+        //예상
+        String title = "K";
+        String content = "321";
+        ArticleForm dto = new ArticleForm(null,title,content);
+        Article expected = new Article(5L,title,content);
+
+        //실제
+        Article article = artcleServie.create(dto);
+
+        //비교
+        assertEquals(expected.toString(),article.toString());
+    }
+
+    @Test
+    void create_fail() {
+        //예상
+        String title = "K";
+        String content = "321";
+        ArticleForm dto = new ArticleForm(3L,title,content);
+        Article expected = null;
+
+        //실제
+        Article article = artcleServie.create(dto);
+
+        //비교
+        assertEquals(expected,article);
+    }
+
+
+}
