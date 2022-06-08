03e6ebb5a663b04b882cf59a2c351165f63195e2
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-06-08 17:44:06 +0900

    add comment layout

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @Slf4j //로깅을 위한 어노테이션
 public class ArticleController {
     @Autowired //스프링 부트가 미리 생성해놓은 객체를 가져다가 자동 연결!
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
     @GetMapping("articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -45,9 +51,10 @@ public class ArticleController {
 
         // 1: id로 데이터를 가져옴
         Article articleEntity = articleRepository.findById(id).orElse(null);
-
+        List<CommentDto> commentDtos = commentService.comments(id);
         // 2: 가져온 데이터를 모델에 등록
         model.addAttribute("article",articleEntity);
+        model.addAttribute("commentDtos",commentDtos);
         // 3. 보여줄 페이지를 설정
 
         return "articles/show";
